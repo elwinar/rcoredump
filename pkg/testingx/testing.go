814dc5043789a0949195d469daf6ea5af9d066fc
Romain Baugue <romain.baugue@elwinar.com>
2020-06-05 23:02:55 +0200

    Correctly commend the testingx package

--- pkg/testingx/testing.go
+++ pkg/testingx/testing.go
@@ -1,3 +1,9 @@
+// testingx contains testing helpers meant to simplify unit testing. Most of
+// the helpers are simple wrapper for other libraries functions with a few
+// tweaks meant to simplify the unit tests:
+// - they don't return an error and instead fail the test,
+// - relative filepath are prefixed by testdata/,
+// - resources like file handles are closed during test cleanup;
 package testingx
 
 import (
@@ -9,14 +15,23 @@ import (
 	"testing"
 )
 
-var Update bool
+// updateGoldenFlag indicates tests to udpate their golden files with the
+// expected output. This flag is controled by the -updategolden flag and will
+// apply to every call to GoldenXXX, one is expected to use the -run flag to
+// limit to specific tests.
+var updateGolden bool
 
 func init() {
-	flag.BoolVar(&Update, "update", false, "update the golden files")
+	flag.BoolVar(&updateGolden, "updategolden", false, "update the golden files")
 }
 
+// Open the file at path and return the handle.
 func Open(t *testing.T, path string) *os.File {
-	f, err := os.Open(filepath.Join(`testdata`, path))
+	t.Helper()
+	if !filepath.IsAbs(path) {
+		path = filepath.Join(`testdata`, path)
+	}
+	f, err := os.Open(path)
 	if err != nil {
 		t.Fatalf(`opening file %q: %s`, path, err)
 	}
@@ -24,21 +39,32 @@ func Open(t *testing.T, path string) *os.File {
 	return f
 }
 
+// ReadFile returns the content of the file at path. See ioutil.ReadFile.
 func ReadFile(t *testing.T, path string) []byte {
-	raw, err := ioutil.ReadFile(filepath.Join(`testdata`, path))
+	t.Helper()
+	if !filepath.IsAbs(path) {
+		path = filepath.Join(`testdata`, path)
+	}
+	raw, err := ioutil.ReadFile(path)
 	if err != nil {
 		t.Fatalf(`reading file %q: %s`, path, err)
 	}
 	return raw
 }
 
+// WriteFile set the content of the file at path. See ioutil.WriteFile.
 func WriteFile(t *testing.T, path string, raw []byte) {
-	err := ioutil.WriteFile(filepath.Join(`testdata`, path), raw, os.ModePerm)
+	t.Helper()
+	if !filepath.IsAbs(path) {
+		path = filepath.Join(`testdata`, path)
+	}
+	err := ioutil.WriteFile(path, raw, os.ModePerm)
 	if err != nil {
 		t.Fatalf(`writing file %q: %s`, path, err)
 	}
 }
 
+// UnmarshalJSON parse the JSON raw string into dest. See json.Unmarshal.
 func UnmarshalJSON(t *testing.T, raw []byte, dest interface{}) {
 	err := json.Unmarshal(raw, dest)
 	if err != nil {
@@ -46,6 +72,7 @@ func UnmarshalJSON(t *testing.T, raw []byte, dest interface{}) {
 	}
 }
 
+// MarshalJSON encode src into a JSON string. See json.Marshal.
 func MarshalJSON(t *testing.T, src interface{}) []byte {
 	raw, err := json.Marshal(src)
 	if err != nil {
@@ -54,15 +81,20 @@ func MarshalJSON(t *testing.T, src interface{}) []byte {
 	return raw
 }
 
+// Golden returns the content of the file at path, eventually changing them to
+// out beforehand if the -goldenupdate flag was set to true on the command
+// line. See ReadFile and WriteFile.
 func Golden(t *testing.T, path string, out []byte) []byte {
-	if Update {
+	if updateGolden {
 		WriteFile(t, path, out)
 	}
 	return ReadFile(t, path)
 }
 
+// GoldenJSON is like Golden, but keep a JSON representation of the given
+// structs into the file at path.
 func GoldenJSON(t *testing.T, path string, out, dest interface{}) {
-	if Update {
+	if updateGolden {
 		WriteFile(t, path, MarshalJSON(t, out))
 	}
 	UnmarshalJSON(t, ReadFile(t, path), dest)
